09ca6fcc67106c6d9124c2771268bae8e3fa29fe
Tony Phondeth <phondeth@gmail.com>
2021-07-21 13:39:11 -0400

    modified html

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,13 +2,13 @@
 $(document).ready(function(){
     
     // Display current date
-    var currentDateEl = moment().format("dddd MMMM D YYYY");
+    var currentDateEl = moment().format("dddd MMMM D, YYYY");
     $('#currentDay').text(currentDateEl);
 
     // Time blocks change color based on hour of day
     var hourEl = moment().get("hour");
     $('.hour').each( function() {
-        var timeBlock = parseInt($(this).attr('value'));
+        var timeBlock = parseInt($(this).attr("value"));
         if (timeBlock < hourEl) {
             $(this).addClass("past");
         }
@@ -19,4 +19,8 @@ $(document).ready(function(){
             $(this).addClass("future");
         }
     });
+
+
+    
+    
 });
