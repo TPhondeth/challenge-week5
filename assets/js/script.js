65b6966fbc5d233635c48e6e44c1654bdb8c3104
Tony Phondeth <phondeth@gmail.com>
2021-07-21 11:14:16 -0400

    added color change to timeblocks

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,21 @@
 $(document).ready(function(){
     
     // Display current date
-    var currentDate = moment().format("dddd MMMM D YYYY");
-    $('#currentDay').text(currentDate);
+    var currentDateEl = moment().format("dddd MMMM D YYYY");
+    $('#currentDay').text(currentDateEl);
+
+    // Time blocks change color based on hour of day
+    var hourEl = moment().get("hour");
+    $('.hour').each( function() {
+        var timeBlock = parseInt($(this).attr('value'));
+        if (timeBlock < hourEl) {
+            $(this).addClass("past");
+        }
+        if (timeBlock === hourEl) {
+            $(this).addClass("present");
+        }
+        if (timeBlock > hourEl) {
+            $(this).addClass("future");
+        }
+    });
 });
